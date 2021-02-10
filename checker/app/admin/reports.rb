5f74e90353a4cfa24a3bbcb7ee118ade9562dc26
n0b0dy-su <isrgm.00@gmail.com>
2021-02-09 19:07:41 -0600

    create| absences reports

--- checker/app/admin/reports.rb
+++ checker/app/admin/reports.rb
@@ -2,7 +2,9 @@ ActiveAdmin.register_page "Reports" do
     controller do
         #definir metodo con joins para obtener las tablas de consulta checks and users
         def index
-            @checks_per_day = User.joins(:checks).where(' type_check = 2') 
+            #@checks_per_day = User.joins(:checks).select("users.name,users.private_number,checks.created_at").where("checks.type_check = 2")
+
+            @month_absences = ActiveRecord::Base.connection.execute("SELECT * FROM absences(2);")
         end     
     end
     content only: :index do
